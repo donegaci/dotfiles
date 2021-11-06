21bb7209c62939e2a81f6ae026593566dfd8497b
Ciaran Donegan <38507438+donegaci@users.noreply.github.com>
2021-11-06 22:16:36 +0000

    Downloading zsh-autosuggestions and zsh-syntax-highlighting

--- install.sh
+++ install.sh
@@ -13,7 +13,12 @@ echo "Installing oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ZSH="$HOME/.dotfiles/oh-my-zsh" sh install.sh
 
-echo "Installing powerlevel10k"
+echo "Downloading powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
 
+echo "Downloading zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+echo "Downloading zsh-syntax-highlightin"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 echo "Done."
