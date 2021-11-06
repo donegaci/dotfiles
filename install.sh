695b411ae90aea67a0871aa9fcfc9d028471aa59
Ciaran Donegan <38507438+donegaci@users.noreply.github.com>
2021-11-06 22:24:01 +0000

    check if plugin exists already

--- install.sh
+++ install.sh
@@ -13,12 +13,19 @@ echo "Installing oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ZSH="$HOME/.dotfiles/oh-my-zsh" sh install.sh
 
-echo "Downloading powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+  echo "Downloading powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 
-echo "Downloading zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+  echo "Downloading zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
 
-echo "Downloading zsh-syntax-highlightin"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  echo "Downloading zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+
 echo "Done."
