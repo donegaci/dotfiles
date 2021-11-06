c754a449e602820a97742affa30b644a77cee5cb
Ciaran Donegan <38507438+donegaci@users.noreply.github.com>
2021-11-06 22:25:37 +0000

    correcting paths

--- install.sh
+++ install.sh
@@ -15,17 +15,17 @@ ZSH="$HOME/.dotfiles/oh-my-zsh" sh install.sh
 
 if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
   echo "Downloading powerlevel10k"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.dotfiles/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 
 if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
   echo "Downloading zsh-autosuggestions"
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.dotfiles/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 
 if [ ! -d "$HOME/.dotfiles/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
   echo "Downloading zsh-syntax-highlighting"
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
 echo "Done."
