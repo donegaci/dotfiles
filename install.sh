ef8e1c16e734a33cf7642f027a966ee94567cb44
Ciaran Donegan <38507438+donegaci@users.noreply.github.com>
2021-11-06 22:03:47 +0000

    install.sh will clone powerlevel10k and install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Symlinking your dotfiles"
 
 cd ~/
@@ -11,5 +12,8 @@ ln -sf .dotfiles/.zshrc
 echo "Installing oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ZSH="$HOME/.dotfiles/oh-my-zsh" sh install.sh
+
+echo "Installing powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "Done."
