4c90d7399e563eb8ca7108fe2a1a3581231660d8
Ciaran Donegan <38507438+donegaci@users.noreply.github.com>
2021-11-06 21:58:35 +0000

    install script downloads oh-my-zsh and installs it in .dotfiles

--- install.sh
+++ install.sh
@@ -8,4 +8,8 @@ ln -sf .dotfiles/.gitconfig
 ln -sf .dotfiles/.bashrc
 ln -sf .dotfiles/.zshrc
 
+echo "Installing oh-my-zsh"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH="$HOME/.dotfiles/oh-my-zsh" sh install.sh
+
 echo "Done."
